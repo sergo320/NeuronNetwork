08898f52b0130abf3402d1e89104136b9d84e519
sergo <devsergo3@gmail.com>
2020-08-20 22:03:43 +0300

    init commit

---
+++ Network/Neuron.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    public class Neuron
+    {
+        public List<double> Weight { get; }
+        public NeuronType Type { get; }
+        public double Output { get; private set; }
+
+    }
+}
